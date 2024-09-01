3f753d5959decb6b0bbd23d08480907d7df11274
limda00 <dylim7715@gmail.com>
2024-09-01 17:05:19 +0900

    update :: 사용자 정보 저장 수정

--- src/model/repository.js
+++ src/model/repository.js
@@ -1,16 +1,21 @@
-const { OP, QueryType } = require("sequelize")
-const { user, sequelize } = require("./")
+const { OP, QueryType } = require("sequelize");
+const { user, sequelize } = require("./");
 
 const saveUser = async (id, password) => {
-    if(!id || !password) {
-        return "ERROR"
-    }
+  if (!id || !password) {
+    return "ERROR";
+  }
 
-    const result = await user.create({user_id: id, password: password})
+  const result = await user.create({
+    email: email,
+    user_name: user_name,
+    user_id: id,
+    password: password,
+  });
 
-    return result
-}
+  return result;
+};
 
 module.exports = {
-    saveUser
-}
+  saveUser,
+};
