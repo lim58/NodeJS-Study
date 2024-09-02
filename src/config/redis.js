2807309c3d7eaab2796668c70f7ad291c23d00a8
limda00 <dylim7715@gmail.com>
2024-09-03 07:34:32 +0900

    update :: 콘솔 로그 문구 수정

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,7 +3,7 @@ const { createClient } = require("redis")
 const redisCli = new createClient({ legacyMode : true })
 
 redisCli.on('connect', () => {
-    console.log('Redis has initted')
+    console.log('Redis has initd')
 })
 redisCli.on('error', err => {
     console.error(err)
